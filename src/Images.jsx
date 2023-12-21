c9ab3674696667b57f1c620929ff68f6894cc023
Bonioszko <bartek.pers@gmail.com>
2023-12-21 15:59:10 +0100

    added photos, grid

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,22 +1,50 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
+import "./Image.css";
 const Images = () => {
   const images = [];
-
+  const [selectedImage, setSelectedImage] = useState("");
+  const [clicked, setClicked] = useState(false);
   const { location } = useParams();
   const path = `./Images/${location}`;
   images.push(path + "/Image1.jpg");
-  console.log(images);
+  images.push(path + "/Image2.jpg");
+  images.push(path + "/Image3.jpg");
+  images.push(path + "/Image4.jpg");
+  images.push(path + "/Image5.jpg");
+  images.push(path + "/Image6.jpg");
+  images.push(path + "/Image7.jpg");
+  images.push(path + "/Image8.jpg");
 
+  const handleImageClick = (event) => {
+    const src = event.target.src;
+    setSelectedImage(src);
+    setClicked(true);
+  };
+  const handlePageClick = () => {
+    setClicked(false);
+  };
   return (
     <>
-      <div className="main">
-        <div className="content">
-          {location}
-          <img src={images[0]} alt="pozdro" />
+      <div className="main-content">
+        <div className={"gallery" + (clicked ? " blur" : "")}>
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt="pozdro"
+              onClick={handleImageClick}
+            />
+          ))}
         </div>
       </div>
+      <img
+        src={selectedImage}
+        alt="do not have"
+        className={clicked ? "clicked" : "hide"}
+        onClick={handlePageClick}
+      ></img>
     </>
   );
 };
--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -17,6 +17,7 @@ function Location({ location }) {
     iframeUrl =
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d77899.03511218326!2d16.745787785931473!3d52.400426047807485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470444d2ece10ab7%3A0xa4ea31980334bfd1!2zUG96bmHFhA!5e0!3m2!1spl!2spl!4v1703100090900!5m2!1spl!2spl";
   }
+
   return (
     <>
       <div className="location">
