26e7c0b2c6eee6f0603acddb36a204fa310b85b0
Bonioszko <bartek.pers@gmail.com>
2023-12-22 21:13:45 +0100

    mobile display fixed

--- src/components/Present.jsx
+++ src/components/Present.jsx
@@ -2,18 +2,22 @@ import { Link } from "react-router-dom";
 import "./Present.css";
 const Present = () => {
   return (
-    <div className="present">
-      <div className="lid">
-        <span></span>
-      </div>
-      <div className="promo">
-        <Link to="/Gift/Maps" className="link">
-          <h2>Mały prezent</h2>
-        </Link>
-      </div>
-      <div className="box">
-        <span></span>
-        <span></span>
+    <div className="pyro">
+      <div className="before"></div>
+      <div className="after"></div>
+      <div className="present">
+        <div className="lid">
+          <span></span>
+        </div>
+        <div className="promo">
+          <Link to="/Gift/Maps" className="link">
+            <h2>Mały prezent</h2>
+          </Link>
+        </div>
+        <div className="box">
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   );
